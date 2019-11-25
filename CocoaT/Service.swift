bd92cc92c1a7e635aca9c4b61cb1ad30d0680110
Teodora Garasanin <t.garasanin@levi9.com>
2019-11-25 14:10:55 +0100

    Add service public class

---
+++ CocoaT/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  CocoaT
+//
+//  Created by Teodora Garasanin on 11/25/19.
+//  Copyright © 2019 Talpa Technology B.V. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    
+    public func test() {
+        print("bojfhwfhi")
+    }
+    
+}
+
