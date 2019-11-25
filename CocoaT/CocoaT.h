c210d5bec15f13f30c326f3f97bc10788d547847
Teodora Garasanin <t.garasanin@levi9.com>
2019-11-25 13:55:49 +0100

    Add empty framwork

---
+++ CocoaT/CocoaT.h
@@ -0,0 +1,19 @@
+//
+//  CocoaT.h
+//  CocoaT
+//
+//  Created by Teodora Garasanin on 11/25/19.
+//  Copyright © 2019 Talpa Technology B.V. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CocoaT.
+FOUNDATION_EXPORT double CocoaTVersionNumber;
+
+//! Project version string for CocoaT.
+FOUNDATION_EXPORT const unsigned char CocoaTVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaT/PublicHeader.h>
+
+
